7d8c5f7a80be4e2a6136727ad05f77a322cbb648
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2018-11-27 11:47:38 +0100

    Initial commit

---
+++ lib/zpl/scaler.rb
@@ -0,0 +1,8 @@
+require "zpl/scaler/version"
+
+module Zpl
+  module Scaler
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/zpl/scaler/version.rb
@@ -0,0 +1,5 @@
+module Zpl
+  module Scaler
+    VERSION = "0.1.0"
+  end
+end
