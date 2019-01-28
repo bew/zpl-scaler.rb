ae1f6e12da98e24e2a39698b9f748e6cfe5ccda8
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2019-01-28 13:29:22 +0100

    Add specs for the scaler

--- spec/zpl-scaler/scaler_spec.rb
+++ spec/zpl-scaler/scaler_spec.rb
@@ -3,3 +3,33 @@ RSpec.describe ZplScaler do
     expect(ZplScaler::VERSION).not_to be nil
   end
 end
+
+RSpec.describe ZplScaler::Scaler do
+  it "1:1 scale gives same zpl code" do
+    code = "^MN42,10^BCA,10"
+
+    expect(ZplScaler::Scaler.ratio_scale(code, 1.0)).to eq code
+  end
+
+  it "2:1 scale" do
+    code = "^MN42,10^BC42,10"
+    scaled_code = "^MN42,20^BC42,20"
+
+    expect(ZplScaler::Scaler.ratio_scale(code, 2.0)).to eq scaled_code
+  end
+
+  def self.it_scales_by_2_cmd(cmd_name, input_args, scaled_args)
+    it "scales 2:1 cmd ^#{ cmd_name }" do
+      input_code = "^#{ cmd_name }#{ input_args }"
+      scaled_code = "^#{ cmd_name }#{ scaled_args }"
+      expect(ZplScaler::Scaler.ratio_scale(input_code, 2.0)).to eq scaled_code
+    end
+  end
+
+  it_scales_by_2_cmd "MN", "1,1", "1,2"
+  it_scales_by_2_cmd "BY", "1,1,1", "2,1,2"
+  it_scales_by_2_cmd "FO", "1,1,1", "2,2,1"
+  it_scales_by_2_cmd "B2", "1,1,1", "1,2,1"
+  it_scales_by_2_cmd "GB", "1,1,1,1,1", "2,2,2,1,1"
+  it_scales_by_2_cmd "BC", "1,1,1,1,1,1", "1,2,1,1,1,1"
+end
