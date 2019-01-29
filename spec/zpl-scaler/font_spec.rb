add252f5bb132c3480bc5ab2c89f9da03fff4379
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2019-01-29 14:39:18 +0100

    Add more font specs on normalization

--- spec/zpl-scaler/font_spec.rb
+++ spec/zpl-scaler/font_spec.rb
@@ -1,7 +1,23 @@
 RSpec.describe ZplScaler::Font do
-  it "normalizes given H/W to font's H/W" do
-    font = ZplScaler::Font.new name: "ZZZ", base_height: 42, base_width: 42, type: :foo, scalable: false
+  it "can differenciate scalable vs non-scalable font" do
+    non_scalable_font = ZplScaler::Font.new name: "ZZZ", base_height: 42, base_width: 42, type: :foo
+    expect(non_scalable_font.scalable?).to be false
 
-    expect(font.normalize_size(height: 0, width: 23)).to eq [42, 42]
+    scalable_font = ZplScaler::Font.new name: "ZZZ", base_height: 42, base_width: 42, type: :foo, scalable: true
+    expect(scalable_font.scalable?).to be true
+  end
+
+  it "normalizes given H/W to font's H/W" do
+    font = ZplScaler::Font.new name: "ZZZ", base_height: 10, base_width: 10, type: :foo
+
+    expect(font.normalize_size(height: 0, width: 7)).to eq [10, 10]
+    expect(font.normalize_size(height: 10, width: 10)).to eq [10, 10]
+  end
+
+  it "normalizes with same multiplication factor for each H and W" do
+    font = ZplScaler::Font.new name: "ZZZ", base_height: 15, base_width: 12, type: :foo
+
+    expect(font.normalize_size(height: 16, width: 12)).to eq [30, 12]
+    expect(font.normalize_size(height: 32, width: 43)).to eq [45, 48]
   end
 end
