d3b48c67df1c1fd850c4709a4a6ba311f42375b1
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2019-01-30 18:12:55 +0100

    Rm keyword-only ratio param, positional is enough

--- spec/zpl-scaler/transformers/font_scaler_spec.rb
+++ spec/zpl-scaler/transformers/font_scaler_spec.rb
@@ -2,12 +2,12 @@ RSpec.describe ZplScaler::Transformer::FontScaler do
   it "doesn't change non-font cmd" do
     non_font_zpl = "^MN42,10"
 
-    tr = ZplScaler::Transformer::FontScaler.new(ratio: 2.0)
+    tr = ZplScaler::Transformer::FontScaler.new(2.0)
     expect(tr.apply(non_font_zpl)).to eq non_font_zpl
   end
 
   it "1:1 scale normalize font sizes" do
-    tr = ZplScaler::Transformer::FontScaler.new(ratio: 1.0, allow_font_change: false)
+    tr = ZplScaler::Transformer::FontScaler.new(1.0, allow_font_change: false)
 
     expect(tr.apply("^AA,2,3")).to eq "^AA,9,5"
     expect(tr.apply("^AA,9,5")).to eq "^AA,9,5"
@@ -21,7 +21,7 @@ RSpec.describe ZplScaler::Transformer::FontScaler do
       input_zpl = "^A#{font.name},1,1"
       normalized_font_zpl = "^A#{font.name},#{font.base_height},#{font.base_width}"
 
-      tr = ZplScaler::Transformer::FontScaler.new(ratio: 1.0, allow_font_change: false)
+      tr = ZplScaler::Transformer::FontScaler.new(1.0, allow_font_change: false)
       expect(tr.apply(input_zpl)).to eq normalized_font_zpl
     end
   end
@@ -30,7 +30,7 @@ RSpec.describe ZplScaler::Transformer::FontScaler do
     it "doesn't normalize scalable font '#{font.name}' to base HxW: #{font.base_height}x#{font.base_width}" do
       input_zpl = "^A#{font.name},42,42"
 
-      tr = ZplScaler::Transformer::FontScaler.new(ratio: 1.0)
+      tr = ZplScaler::Transformer::FontScaler.new(1.0)
       expect(tr.apply(input_zpl)).to eq input_zpl
     end
   end
@@ -38,7 +38,7 @@ RSpec.describe ZplScaler::Transformer::FontScaler do
   it "doesn't change partial font cmd" do
     partial_font_cmd = "^CFD,,12" # font: D, height: unspecified, width: 12
 
-    tr = ZplScaler::Transformer::FontScaler.new(ratio: 1.0)
+    tr = ZplScaler::Transformer::FontScaler.new(1.0)
     expect(tr.apply(partial_font_cmd)).to eq partial_font_cmd
   end
 
@@ -46,14 +46,14 @@ RSpec.describe ZplScaler::Transformer::FontScaler do
     # Font '0' is a scalable font
 
     code = "^A0,7,7"
-    tr = ZplScaler::Transformer::FontScaler.new(ratio: 2.0)
+    tr = ZplScaler::Transformer::FontScaler.new(2.0)
     expect(tr.apply(code)).to eq "^A0,14,14"
   end
 
   it "scaling changes to a smaller font when allowed" do
     code = "^AD,7,7"
 
-    tr = ZplScaler::Transformer::FontScaler.new(ratio: 1.5)
+    tr = ZplScaler::Transformer::FontScaler.new(1.5)
     expect(tr.apply(code)).to eq "^AA,27,15"
     # What happens here:
     # → 7,7 is normalized to 18,10
